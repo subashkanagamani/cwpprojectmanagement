e209c2f4b13ac0afb1a9872c55e02bfc7c950766
veogemini96-bit <veogemini96@gmail.com>
2026-02-08 15:41:19 +0530

    Updated vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -5,6 +5,7 @@ import path from "path";
 export default defineConfig({
   plugins: [react()],
   root: path.resolve(__dirname, "client"),
+  envDir: path.resolve(__dirname),
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "client/src"),
